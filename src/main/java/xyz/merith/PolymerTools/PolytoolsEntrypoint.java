3b5ada59411ba6549a1c46094c4affcdec99213e
Merith-TK <merith.tk@gmail.com>
2024-07-16 19:41:55 -0700

    fix tool values being incorrect

--- src/main/java/xyz/merith/PolymerTools/PolytoolsEntrypoint.java
+++ src/main/java/xyz/merith/PolymerTools/PolytoolsEntrypoint.java
@@ -1,5 +1,6 @@
 package xyz.merith.PolymerTools;
 
+import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
 import net.fabricmc.api.ModInitializer;
 
 import net.minecraft.item.*;
@@ -18,20 +19,47 @@ public class PolytoolsEntrypoint implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("polytools");
 
 
+	// TODO: Copper Armor
+	// TODO: Emerald Tools
+	// TODO: Test Blocks
+
+
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Loading Polymer Tools");
+
 		// Register Tools
 		Registry.register(Registries.ITEM, Identifier.of("polytools", "copper_axe"),
-				new CopperAxe(	Items.IRON_AXE,		CopperMaterial.INSTANCE, new Item.Settings()));
+				new CopperAxe(
+						Items.IRON_AXE,
+						PolyToolMaterials.COPPER,
+						new Item.Settings()
+								.attributeModifiers(AxeItem.createAttributeModifiers(PolyToolMaterials.COPPER, 6.5F, -3.15F))));
 		Registry.register(Registries.ITEM, Identifier.of("polytools", "copper_hoe"),
-				new CopperHoe(	Items.IRON_HOE,		CopperMaterial.INSTANCE, new Item.Settings()));
+				new CopperHoe(
+						Items.IRON_HOE,
+						PolyToolMaterials.COPPER,
+						new Item.Settings()
+								.attributeModifiers(HoeItem.createAttributeModifiers(PolyToolMaterials.COPPER, -1.5F, -2.5F))));
 		Registry.register(Registries.ITEM, Identifier.of("polytools", "copper_pickaxe"),
-				new CopperPickaxe(Items.IRON_PICKAXE,	CopperMaterial.INSTANCE, new Item.Settings()));
+				new CopperPickaxe(
+						Items.IRON_PICKAXE,
+						PolyToolMaterials.COPPER,
+						new Item.Settings()
+								.attributeModifiers(PickaxeItem.createAttributeModifiers(PolyToolMaterials.COPPER, 1.0F, -2.8F))));
 		Registry.register(Registries.ITEM, Identifier.of("polytools", "copper_shovel"),
-				new CopperShovel(	Items.IRON_SHOVEL,	CopperMaterial.INSTANCE, new Item.Settings()));
+				new CopperShovel(
+						Items.IRON_SHOVEL,
+						PolyToolMaterials.COPPER,
+						new Item.Settings()
+								.attributeModifiers(ShovelItem.createAttributeModifiers(PolyToolMaterials.COPPER, 1.5F, -3.0F))));
 		Registry.register(Registries.ITEM, Identifier.of("polytools", "copper_sword"),
-				new CopperSword(	Items.IRON_SWORD,	CopperMaterial.INSTANCE, new Item.Settings()));
+				new CopperSword(
+						Items.IRON_SWORD,
+						PolyToolMaterials.COPPER,
+						new Item.Settings()
+								.attributeModifiers(SwordItem.createAttributeModifiers(PolyToolMaterials.COPPER, 3, -2.4F))));
+		PolymerResourcePackUtils.addModAssets("polytools");
 		LOGGER.info("Done");
 	}
 }
